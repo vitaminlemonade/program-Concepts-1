e08526ff0c5d21782d9ba2e6b891e5988cd245f3
joramas <oramas.29@hotmail.com>
2023-04-14 12:36:45 -0400

    Supporting functions implementations

--- A5Linj&OramasJ/A5Linj&OramasJ/Customer.cs
+++ A5Linj&OramasJ/A5Linj&OramasJ/Customer.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/* Customer.cs
+ * A class representing a customer for the database
+ * 
+ * Revision History:  
+ *      Jiadong Lin, 2023.04.12: Created
+ *      Jose Oramas, 2023.04.14: Implemented customer's main functionality
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +16,43 @@ namespace A5Linj_OramasJ
 {
     internal class Customer
     {
-        public string email, name;
-        public int age;
+        private string m_email = "", m_name = "";
+        private int m_id = -1, m_age = -1;
+
+        // NOTE: Default constructor
+        public Customer()
+        {
+            m_id = -1;
+            m_name = "";
+            m_email = "";
+            m_age = -1;
+        }
+
+        public Customer(int id, string name, int age, string email)
+        {
+            m_id = id;
+            m_name = name;
+            m_age = age;
+            m_email = email;
+        }
+
+        public int GetId() { return m_id;  }
+        public string GetEmail() { return m_email; }
+
+        public void SetName(string name) { m_name = name; }
+        public void SetAge(int age) { m_age = age; }
+        public void SetEmail(string email) { m_email = email; }
+
+        public void DisplayCustomerInformation()
+        {
+            Console.WriteLine($"Customer {m_id}: {m_name}, {m_age} years, email: {m_email}.");
+        }
+
+        public void EditCustomerInformation(string name, int age, string email)
+        {
+            m_name = name;
+            m_age = age;
+            m_email = email;
+        }
     }
 }
--- A5Linj&OramasJ/A5Linj&OramasJ/Program.cs
+++ A5Linj&OramasJ/A5Linj&OramasJ/Program.cs
@@ -2,8 +2,9 @@
  * Name of Project (File):A5LinJ&OramasJ
  * Purpose of Program: For dealer who only sells three car brands. When the program starts, get the name of each of the three brands from the user and store them.
  * Revision History:
- *  Jiadong Lin, 2023.04.12: Created
- *  
+ *      Jiadong Lin, 2023.04.12: Created
+ *      Jose Oramas, 2023.04.14: AddCustomer, EditCustomer, CheckEmail implementations
+ *      Jiadong Lin, 2023.04.14: DisplayCustomers implementation
  */
 using System;
 using System.Collections.Generic;
@@ -16,7 +17,68 @@ namespace A5Linj_OramasJ
 {
     internal class Program
     {
+        // NOTE: Customers database
         static List<Customer> customers = new List<Customer>();
+        // NOTE: This is used for customer id generation
+        static int idGen = 1000;
+
+        static bool CheckEmail(string email)
+        {
+            bool result = false;
+            int indexAt = -1;
+            int indexLastDot = -1;
+
+            for(int i = 0; i < email.Length; ++i)
+            {
+                char c = email[i];
+
+                if(c == '@')
+                {
+                    indexAt = i;
+                }
+
+                if(c == '.' && indexLastDot < i)
+                {
+                    indexLastDot = i;
+                }
+            }
+
+            if(indexAt != -1 && indexLastDot != -1 && (indexAt < indexLastDot))
+            {
+                // NOTE: Check if the email already exists in the data base
+                bool notFound = true;
+                for(int i = 0; i < customers.Count && notFound; ++i)
+                {
+                    Customer c = customers[i];
+                    if(c.GetEmail() == email)
+                    {
+                        notFound = false;
+                    }
+                }
+
+                if (notFound)
+                {
+                    result = true;
+                }
+                else
+                {
+                    Console.WriteLine("The email already exists!");
+                    Console.WriteLine("Customer was not added!");
+                    Console.WriteLine("Press any key to continue... ");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("email was not in the correct format");
+                Console.WriteLine("Customer was not added!");
+                Console.WriteLine("Press any key to continue... ");
+                Console.ReadKey();
+            }
+               
+            return result;
+        }
+
         static void Main(string[] args)
         {
             int choice;
@@ -28,7 +90,7 @@ namespace A5Linj_OramasJ
                 Console.WriteLine("Select an options (only 1 to 5)");
                 Console.WriteLine("1. Add New Customer");
                 Console.WriteLine("2. Edit Existing Customer");
-                Console.WriteLine("3. Display Customer");
+                Console.WriteLine("3. Display Customers");
                 Console.WriteLine("4. Exit");
                 choice = GetInt("");
                 if (choice == 1)
@@ -37,26 +99,166 @@ namespace A5Linj_OramasJ
                 }
                 else if (choice == 2)
                 {
-                    EditExistingCustomer();
+                    int id;
+                    int index = -1;
+                    if (DisplayCustomers() > 0)
+                    {
+                        id = GetInt("Which customer do you want to edit? id: ");
+                        index = EditExistingCustomer(id);
+                        if(index != -1)
+                        {
+                            Console.WriteLine("NEW CUSTOMER DATA:");
+                            customers[index].DisplayCustomerInformation();
+                            Console.ReadKey();
+                        }
+                    }
                 }
                 else if (choice == 3)
                 {
-                    DisplayCustomer();
+                    if(DisplayCustomers() > 0)
+                    {
+                        // NOTE: This is to avoid clearing the console before watching the customers
+                        Console.Write("\nPress any key to continue... ");
+                        Console.ReadKey();
+                    }
                 }
             } while (choice <= 1 || choice <= 3 || choice > 4);
         }
+
         static void AddNewCustomer()
         {
+            string name;
+            string email;
+            int age;
 
+            bool isEmailOk;
+
+            // NOTE: Get the name
+            Console.Write("What's the customer name? ");
+            name = Console.ReadLine();
+
+            // NOTE: Get the age (must be in the correct range)
+            do
+            {
+                age = GetInt("What's the customer age? ");
+                if(age < 13 || age > 100)
+                {
+                    Console.WriteLine("Age must be between 13 and 100 years.");
+                    Console.WriteLine("Press any key to continue... ");
+                    Console.ReadKey();
+                }
+            } while (age < 13 || age > 100);
+
+            // NOTE: Get the email (email must be in the correct format)
+            // NOTE: email must not be present in the customers list
+            Console.Write("What's the customer email? ");
+            email = Console.ReadLine();
+
+            isEmailOk = CheckEmail(email);
+          
+            if (isEmailOk)
+            {
+                Customer newCustomer = new Customer(idGen++, name, age, email);
+                customers.Add(newCustomer);
+            }
         }
-        static void EditExistingCustomer()
+
+        // NOTE: it returns the index in the database of the edited customer for displaying pursposes
+        static int EditExistingCustomer(int id)
         {
+            int index = -1; ;
+            bool found = false;
+            string answer = "";
+            string newName = "";
+            string newEmail = "";
+            int newAge = -1;
 
+            for(int i = 0; i < customers.Count; ++i)
+            {
+                if(customers[i].GetId() == id)
+                {
+                    found = true;
+
+                    do
+                    {
+                        Console.Write("What would you like to change? (email, age, name): ");
+                        answer = Console.ReadLine().ToLower();
+
+                        switch(answer)
+                        {
+                            case "name":
+                                {
+                                    Console.Write("New name: ");
+                                    newName = Console.ReadLine();
+                                    customers[i].SetName(newName);
+                                    index = i;
+                                } break;
+
+                            case "age":
+                                {
+                                    do
+                                    {
+                                        newAge = GetInt("New age: ");
+                                        customers[i].SetAge(newAge);
+                                        index = i;
+                                    } while (newAge < 13 || newAge > 100);
+                                }
+                                break;
+
+                            case "email":
+                                {
+                                    Console.Write("New email: ");
+                                    newEmail = Console.ReadLine();
+                                    customers[i].SetEmail(newEmail);
+                                    index = i;
+                                } break;
+
+                            default:
+                                {
+                                    Console.WriteLine("Only name, age or email are accepted!");
+                                    Console.WriteLine("Press any key to continue... ");
+                                    Console.ReadKey();
+                                } break;
+                        }
+
+                    } while (answer != "name" && answer != "age" && answer != "email");
+
+                }
+            }
+
+            if(!found)
+            {
+                Console.WriteLine("Customer cannot be found!");
+                Console.Write("Press any key to continue... ");
+                Console.ReadKey();
+
+                index = -1;
+            }
+
+            return index;
         }
-        static void DisplayCustomer()
+
+        static int DisplayCustomers()
         {
+            if (customers.Count > 0)
+            {
+                Console.WriteLine("\nLIST OF CUSTOMERS:");
+                for (int i = 0; i < customers.Count; ++i)
+                {
+                    Customer customer = customers[i];
+                    customer.DisplayCustomerInformation();
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no customers registered!");
+                Console.Write("Press any key to continue... ");
+                Console.ReadKey();
+            }
 
+            return customers.Count;
         }
+
         static int GetInt(string userPrompt)
         {
             //For int number only
